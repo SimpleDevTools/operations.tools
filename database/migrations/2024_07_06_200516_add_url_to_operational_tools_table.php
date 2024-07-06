3288e61d4c4d75e1df150e4bc5114c262c1adab7
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2024-07-06 17:02:02 -0400

    Fix migration.

--- database/migrations/2024_07_06_200516_add_url_to_operational_tools_table.php
+++ database/migrations/2024_07_06_200516_add_url_to_operational_tools_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('operational_tools', function (Blueprint $table) {
-            $table->string('url');
+            $table->string('url')->nullable();
         });
     }
 
